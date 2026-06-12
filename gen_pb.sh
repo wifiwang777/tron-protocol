bcf635ad90107d7f5d319fd20dbb5924f0207f07
wifi-ubuntu <shaohuahongyan@gmail.com>
2026-06-12 11:08:01 +0800

    update

--- gen_pb.sh
+++ gen_pb.sh
@@ -2,14 +2,13 @@
 
 PROTO_PATH="./"
 OUTPUT_DIR="./protos"
-GOOGLEAPIS_PATH="~/code/protocol-buffer/googleapis"
 mkdir -p "$OUTPUT_DIR"
 
 # 遍历所有 .proto 文件并编译
 for proto_file in $(find "$PROTO_PATH" -name "*.proto"); do
     echo "Compiling: $proto_file"
     protoc  -I . \
-            -I $GOOGLEAPIS_PATH \
+            -I ~/code/protocol-buffer/googleapis \
             --go_out=./protos \
             --go_opt=paths=source_relative \
             --go-grpc_out=./protos \
